c9309ac45b3835082d19fe10a406adead38ed4d7
Will Killian <william.killian@outlook.com>
2024-12-01 10:22:48 -0500

    Add Day01
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- advent.cpp
+++ advent.cpp
@@ -1,27 +1,68 @@
 #include "day.hpp"
 
-namespace day01 {
-using parsed = std::vector<int>;
-using part1_answer = int;
-using part2_answer = int;
+#include <algorithm>
+#include <charconv>
+#include <iterator>
+#include <ranges>
 
-[[nodiscard]] [[gnu::noinline]] static parsed
+namespace day01 {
+using parsed = std::pair<std::vector<int>, std::vector<int>>;
+using part1_answer = int;
+using part2_answer = long;
+
+[[nodiscard]] [[gnu::noinline]] static auto
 do_parse(std::span<char const> bytes) {
-  return {};
+  std::pair<std::vector<int>, std::vector<int>> pair;
+  pair.first.reserve(1000);
+  pair.second.reserve(1000);
+  auto lines = bytes | std::views::split('\n');
+  for (auto l : lines) {
+    if (l.empty()) {
+      break;
+    }
+    auto f1 = l.begin();
+    auto f2 = std::ranges::find(l, ' ');
+    auto s1 = std::ranges::find(std::views::reverse(l), ' ').base();
+    auto s2 = l.end();
+    int f{0}, s{0};
+    std::from_chars(f1.base(), f2.base(), f);
+    std::from_chars(s1.base(), s2.base(), s);
+    pair.first.emplace_back(f);
+    pair.second.emplace_back(s);
+  }
+  std::ranges::sort(pair.first);
+  std::ranges::sort(pair.second);
+  return pair;
 }
 
 [[nodiscard]] [[gnu::noinline]] static part1_answer
 do_part1(parsed const &data) {
-  return {};
+  return std::ranges::fold_left(
+      std::views::zip_transform([](int a, int b) { return std::abs(a - b); },
+                                data.first, data.second),
+      0, std::plus{});
 }
 
 [[nodiscard]] [[gnu::noinline]] static part2_answer
 do_part2(parsed const &data, [[maybe_unused]] part1_answer const &answer) {
-  return {};
+  auto const end = data.second.end();
+  auto curr = data.second.begin();
+  return std::ranges::fold_left(
+      std::views::transform(data.first,
+                            [&](int f) {
+                              // advance second to when they are strictly >=
+                              while (curr != end and *curr < f) {
+                                ++curr;
+                              }
+                              auto old = curr;
+                              while (curr != end and *curr == f) {
+                                ++curr;
+                              }
+                              return f * std::distance(old, curr);
+                            }),
+      0, std::plus{});
 }
 
 } // namespace day01
 
-int main() {
-  SOLVE_DAY(day01);
-}
+int main() { SOLVE_DAY(day01); }
--- day.hpp
+++ day.hpp
@@ -10,11 +10,11 @@
     using namespace Day;                                                       \
     util::buffer input{"inputs/" #Day ".txt"};                                 \
     auto t0 = std::chrono::steady_clock::now();                                \
-    parsed const data = do_parse(input.span);                                  \
+    auto const data = do_parse(input.span);                                    \
     auto t1 = std::chrono::steady_clock::now();                                \
-    part1_answer const part1 = do_part1(data);                                 \
+    auto const part1 = do_part1(data);                                         \
     auto t2 = std::chrono::steady_clock::now();                                \
-    part2_answer const part2 = do_part2(data, part1);                          \
+    auto const part2 = do_part2(data, part1);                                  \
     auto t3 = std::chrono::steady_clock::now();                                \
     std::println("{}: Part1={} Part2={}", #Day, part1, part2);                 \
     auto parse_time =                                                          \
