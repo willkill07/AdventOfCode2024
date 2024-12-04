bde0d7106b0728f63b65be4f8a39fcdc282c653d
Will Killian <william.killian@outlook.com>
2024-12-03 20:08:38 -0500

    Update Days 01,03 with improved parsing + ranges
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- src/day01.cpp
+++ src/day01.cpp
@@ -1,15 +1,17 @@
 module;
 
 #include <algorithm>
-#include <charconv>
 #include <concepts>
+#include <functional>
 #include <ranges>
 #include <string_view>
 #include <vector>
 
+#include <ctre.hpp>
+
 export module day01;
 
-constexpr auto absdiff = []<typename T>(T const& x, std::same_as<T> auto const& y) noexcept {
+constexpr auto AbsDiff = []<typename T>(T const& x, std::same_as<T> auto const& y) noexcept {
   T const diff{x - y};
   return (diff < 0) ? -diff : diff;
 };
@@ -18,25 +20,11 @@ export using Day01ParsedType = std::vector<std::pair<long, long>>;
 export using Day01AnswerType = long;
 
 export Day01ParsedType Day01Parse(std::string_view input) noexcept {
-  Day01ParsedType res;
-  res.reserve(1000);
-  std::from_chars_result r{.ptr = input.data(), .ec = std::errc{}};
-  char const* end{r.ptr + input.size()};
-  while (r.ptr != end) {
-    long f{0}, s{0};
-    // parse the first number
-    if (r = std::from_chars(r.ptr, end, f); r.ec != std::errc{})
-      break;
-    // consume all whitespace
-    while (*++r.ptr == ' ')
-      ;
-    // parse the second number
-    if (r = std::from_chars(r.ptr, end, s); r.ec != std::errc{})
-      break;
-    res.emplace_back(f, s);
-    // increment consumes the newline character
-    ++r.ptr;
-  }
+  auto res = ctre::search_all<R"((\d+) +(\d+))">(input) | std::views::transform([](auto x) {
+               auto [_, a, b] = x;
+               return std::pair{a.template to_number<long>(), b.template to_number<long>()};
+             }) |
+             std::ranges::to<std::vector>();
   std::ranges::sort(std::views::keys(res));
   std::ranges::sort(std::views::values(res));
   return res;
@@ -44,24 +32,24 @@ export Day01ParsedType Day01Parse(std::string_view input) noexcept {
 
 export Day01AnswerType Day01Part1(Day01ParsedType const& data) noexcept {
   return std::ranges::fold_left(
-      std::views::zip_transform(absdiff, std::views::keys(data), std::views::values(data)), 0L, std::plus{});
+      std::views::zip_transform(AbsDiff, std::views::keys(data), std::views::values(data)), 0L, std::plus{});
 }
 
 export Day01AnswerType Day01Part2(Day01ParsedType const& data,
                                   [[maybe_unused]] Day01AnswerType const& answer) {
+  long offset{0};
   std::ranges::random_access_range auto a = std::views::keys(data);
   std::ranges::random_access_range auto b = std::views::values(data);
-  std::random_access_iterator auto ib = b.begin();
+  std::ranges::random_access_range auto check{b | std::views::drop(0)};
+  
   auto compute = [&](long x) noexcept -> long {
-    // advance b until it is strictly >= x
-    while (ib != b.end() and *ib < x) {
-      ++ib;
-    }
-    std::random_access_iterator auto ob = ib;
-    while (ib != b.end() and *ib == x) {
-      ++ib;
-    }
-    return x * std::ranges::distance(ob, ib);
+    auto skip_range = check | std::views::take_while(std::bind_front(std::greater{}, x));
+    long const skip{std::ranges::distance(skip_range)};
+    auto dist_range = check | std::views::drop(skip) | std::views::take_while(std::bind_front(std::equal_to{}, x));
+    long const dist{std::ranges::distance(dist_range)};
+    offset += skip + dist;
+    check = b | std::views::drop(offset);
+    return x * dist;
   };
   return std::ranges::fold_left(std::views::transform(a, compute), 0L, std::plus{});
 }
--- src/day03.cpp
+++ src/day03.cpp
@@ -5,44 +5,38 @@ module;
 #include <string_view>
 
 #include <ctre.hpp>
-#include <ctre/range.hpp>
 
 export module day03;
 
 export using Day03ParsedType = std::string_view;
 export using Day03AnswerType = long;
 
+namespace stdv = std::views;
+
 export Day03ParsedType Day03Parse(std::string_view input) noexcept {
   return input;
 }
 
-constexpr auto DoMultiply = [](auto&& match) {
-  long a{0}, b{0};
-  auto ra = match.template get<"a">();
-  auto rb = match.template get<"b">();
-  std::from_chars(ra.begin(), ra.end(), a);
-  std::from_chars(rb.begin(), rb.end(), b);
-  return a * b;
+constexpr auto DoMultiply = [](auto&& match) -> long {
+  auto [_, a, b] = match;
+  return a.template to_number<long>() * b.template to_number<long>();
 };
 
-constexpr auto FindAllMuls = [](auto range) {
-  return std::views::transform(ctre::search_all<R"(mul\((?<a>\d+),(?<b>\d+)\))">(range),
-                               [](auto&& x) { return DoMultiply(x); });
+constexpr auto GetMuls = [](auto&& range) {
+  return ctre::search_all<R"(mul\((\d+),(\d+)\))">(range) | stdv::transform(DoMultiply);
 };
 
 export Day03AnswerType Day03Part1(Day03ParsedType const& data) {
-  return std::ranges::fold_left(FindAllMuls(data), 0L, std::plus{});
+  return std::ranges::fold_left(GetMuls(data), 0L, std::plus{});
 }
+
+constexpr auto OnlyValid = [](auto&& x) { return ctre::split<R"(don't\(\))">(x) | stdv::take(1); };
+
+constexpr auto FilterValid = [](std::string_view data) {
+  return ctre::split<R"(do\(\))">(data) | stdv::transform(OnlyValid) | stdv::join;
+};
 
 export Day03AnswerType Day03Part2(Day03ParsedType const& data,
                                   [[maybe_unused]] Day03AnswerType const& answer) {
-
-  return std::ranges::fold_left(
-      std::views::join(std::views::transform(
-          std::views::join(std::views::transform(
-              ctre::split<"do\\(\\)">(data),
-              [](auto&& v) { return ctre::split<"don't\\(\\)">(v) | std::views::take(1); })),
-          FindAllMuls)),
-      0L,
-      std::plus{});
+  return std::ranges::fold_left(FilterValid(data) | stdv::transform(GetMuls) | stdv::join, 0L, std::plus{});
 }
