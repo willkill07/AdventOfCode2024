87e014c870926d34e65750596ec5a00b86802a32
Will Killian <william.killian@outlook.com>
2024-12-03 23:06:11 -0500

    Add I/O timing; fix total timing
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- src/advent.cpp
+++ src/advent.cpp
@@ -12,12 +12,20 @@ import util;
 struct TimingStats {
   static inline int repetitions{1};
 
+  long io{0};
   long parse{0};
   long part1{0};
   long part2{0};
   long total{0};
 
+  constexpr TimingStats() noexcept = default;
+
+  TimingStats(long io, long parse, long part1, long part2) noexcept
+      : io{io}, parse{parse}, part1{part1}, part2{part2}, total{io + (parse + part1 + part2) / repetitions} {
+  }
+
   constexpr inline TimingStats& operator+=(TimingStats const& other) noexcept {
+    io += other.io;
     parse += other.parse;
     part1 += other.part1;
     part2 += other.part2;
@@ -39,39 +47,41 @@ template <> struct std::formatter<TimingStats> {
   // NOLINTNEXTLINE(readability-identifier-naming)
   inline auto format(TimingStats const& obj, std::format_context& ctx) const {
     return std::format_to(ctx.out(),
-                          "{:>12}µs{:>12}µs{:>12}µs{:>12}µs",
+                          "{:>10}µs{:>10}µs{:>10}µs{:>10}µs{:>10}µs",
+                          obj.io,
                           obj.parse / TimingStats::repetitions,
                           obj.part1 / TimingStats::repetitions,
                           obj.part2 / TimingStats::repetitions,
-                          obj.total / TimingStats::repetitions);
+                          obj.total);
   }
 };
 
 template <auto ParseFn, auto Part1Fn, auto Part2Fn>
 [[nodiscard]] [[gnu::noinline]] static TimingStats SolveDay(char const* day, char const* filename) noexcept {
   using ClockType = std::chrono::steady_clock;
-  std::string const input{util::ReadFile(filename)};
   ClockType::time_point const t0 = ClockType::now();
+  std::string const input{util::ReadFile(filename)};
+  ClockType::time_point const t1 = ClockType::now();
   auto const data = ParseFn(input);
   for (int i = 1; i < TimingStats::repetitions; ++i) {
     std::ignore = ParseFn(input);
   }
-  ClockType::time_point const t1 = ClockType::now();
+  ClockType::time_point const t2 = ClockType::now();
   auto const part1 = Part1Fn(data);
   for (int i = 1; i < TimingStats::repetitions; ++i) {
     std::ignore = Part1Fn(data);
   }
-  ClockType::time_point const t2 = ClockType::now();
+  ClockType::time_point const t3 = ClockType::now();
   auto const part2 = Part2Fn(data, part1);
   for (int i = 1; i < TimingStats::repetitions; ++i) {
     std::ignore = Part2Fn(data, part1);
   }
-  ClockType::time_point const t3 = ClockType::now();
-  TimingStats const stats{.parse = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count(),
-                          .part1 = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count(),
-                          .part2 = std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2).count(),
-                          .total = std::chrono::duration_cast<std::chrono::microseconds>(t3 - t0).count()};
-  std::println("Day {}:   {:>20}{:>20}{}", day + 3, std::move(part1), std::move(part2), stats);
+  ClockType::time_point const t4 = ClockType::now();
+  TimingStats const stats{/*io=*/std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count(),
+                          /*parse=*/std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count(),
+                          /*part1=*/std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2).count(),
+                          /*part2=*/std::chrono::duration_cast<std::chrono::microseconds>(t4 - t3).count()};
+  std::println("Day {}:   {:>16}{:>16}{}", day + 3, std::move(part1), std::move(part2), stats);
   return stats;
 }
 
@@ -86,16 +96,18 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  std::println("          {:>20s}{:>20s}{:>14s}{:>14s}{:>14s}{:>14s}",
+  std::println("          {:>16s}{:>16s}{:>12s}{:>12s}{:>12s}{:>12s}{:>12s}",
                "Part 1",
                "Part 2",
+               "File I/O",
                "Parse",
                "Part 1",
                "Part 2",
                "Total");
-  std::println("          {:>20s}{:>20s}{:>14s}{:>14s}{:>14s}{:>14s}",
+  std::println("          {:>16s}{:>16s}{:>12s}{:>12s}{:>12s}{:>12s}{:>12s}",
                "======",
                "======",
+               "========",
                "=====",
                "======",
                "======",
@@ -107,5 +119,5 @@ int main(int argc, char* argv[]) {
   stats += SOLVE_DAY(Day02);
   stats += SOLVE_DAY(Day03);
 
-  std::println("{:<10s}{:>20}{:>20}{}", "Total:", " --- ", " --- ", stats);
+  std::println("{:<10s}{:>16s}{:>16s}{}", "Total:", " --- ", " --- ", stats);
 }
