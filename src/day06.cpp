adbb5be9ce6f9f510eba93b2386d672dc14e72e5
Will Killian <william.killian@outlook.com>
2024-12-08 19:16:30 -0500

    Update Day06 -- correctness issue for part 2
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- src/day06.cpp
+++ src/day06.cpp
@@ -1,149 +1,321 @@
 module;
 
 #include <algorithm>
-#include <bit>
-#include <cstdint>
-#include <string>
+#include <atomic>
+#include <ranges>
 #include <string_view>
+#include <thread>
 #include <utility>
 
 #include <ankerl/unordered_dense.h>
-#include <unordered_set>
+#include <thread_pool/thread_pool.h>
 
 export module day06;
 
-struct Guard {
-  int x{0}, y{0}, dx{0}, dy{-1};
+namespace {
 
-  constexpr Guard() noexcept = default;
+enum class Dir { Up = 0, Right = 1, Down = 2, Left = 3 };
 
-  constexpr inline Guard(int x, int y) noexcept : x{x}, y{y} {
+struct Point {
+  int x, y;
+
+  constexpr inline Point(int x, int y) noexcept : x{x}, y{y} {
+  }
+  constexpr inline Point(Dir dir) noexcept : Point(0, 0) {
+    switch (dir) {
+    case Dir::Up:
+      y = -1;
+      break;
+    case Dir::Down:
+      y = 1;
+      break;
+    case Dir::Left:
+      x = -1;
+      break;
+    case Dir::Right:
+      x = 1;
+      break;
+    }
   }
 
-  constexpr inline void Turn() noexcept {
-    std::swap(dx, dy);
-    dx = -dx;
-  }
-
-  constexpr inline Guard& Move() noexcept {
-    std::tie(x, y) = std::pair(x + dx, y + dy);
+  constexpr inline Point& operator+=(Point const& p) noexcept {
+    x += p.x;
+    y += p.y;
     return *this;
   }
 
-  constexpr bool operator==(Guard const& rhs) const noexcept {
-    return std::bit_cast<__int128_t>(*this) == std::bit_cast<__int128_t>(rhs);
+  constexpr inline Point& operator+=(Dir dir) noexcept {
+    return *this += Point{dir};
+    switch (dir) {
+    case Dir::Down:
+      ++y;
+      break;
+    case Dir::Up:
+      --y;
+      break;
+    case Dir::Left:
+      --x;
+      break;
+    case Dir::Right:
+      ++x;
+      break;
+    }
+    return *this;
+  }
+
+  constexpr inline Point& operator-=(Dir dir) noexcept {
+    return *this -= Point{dir};
+    switch (dir) {
+    case Dir::Down:
+      --y;
+      break;
+    case Dir::Up:
+      ++y;
+      break;
+    case Dir::Left:
+      ++x;
+      break;
+    case Dir::Right:
+      --x;
+      break;
+    }
+    return *this;
+  }
+
+  constexpr inline Point& operator-=(Point const& p) noexcept {
+    x -= p.x;
+    y -= p.y;
+    return *this;
+  }
+
+  [[nodiscard]] constexpr inline Point operator+(Point const& p) const noexcept {
+    return Point{*this} += p;
+  }
+
+  [[nodiscard]] constexpr inline Point operator+(Dir dir) const noexcept {
+    return Point{*this} += dir;
+  }
+
+  [[nodiscard]] constexpr inline Point operator-(Point const& p) const noexcept {
+    return Point{*this} -= p;
+  }
+
+  [[nodiscard]] constexpr inline Point operator-(Dir dir) const noexcept {
+    return Point{*this} -= dir;
+  }
+
+  [[nodiscard]] constexpr inline bool operator==(Point const& rhs) const noexcept = default;
+
+  [[nodiscard]] constexpr inline std::size_t Index(auto dim) const noexcept {
+    return static_cast<std::size_t>(dim) * static_cast<std::size_t>(y) + static_cast<std::size_t>(x);
   }
 };
 
-struct GuardHash {
-  using is_avalanching = void; //NOLINT(readability-identifier-naming)
+struct Guard {
+  Point loc{0, 0};
+  Dir dir{Dir::Up};
 
-  [[nodiscard]] auto operator()(Guard const& g) const noexcept -> std::size_t {
-    static_assert(std::has_unique_object_representations_v<Guard>);
-    return ankerl::unordered_dense::detail::wyhash::hash(&g, sizeof(g));
+  constexpr Guard() noexcept = default;
+
+  constexpr inline Guard(Point const& l, Dir const& d = Dir::Up) noexcept : loc{l}, dir{d} {
   }
+
+  constexpr inline void Turn() noexcept {
+    dir = Dir{(std::to_underlying(dir) + 1) & 0x3};
+  }
+
+  constexpr inline Guard& Move() noexcept {
+    loc += dir;
+    return *this;
+  }
+
+  [[nodiscard]] constexpr inline std::size_t Index(auto dim) const noexcept {
+    return loc.Index(dim) * 4LU + static_cast<std::size_t>(std::to_underlying(dir));
+  }
+
+  constexpr bool operator==(Guard const& rhs) const noexcept = default;
 };
 
-struct Context {
-  std::string buf;
-  int dim;
-  Guard g;
+class JumpMap {
+public:
+  std::size_t dim;
+  Guard guard;
 
-  constexpr Context() noexcept = default;
-  Context(std::string_view data) noexcept
-      : buf{data}, dim{static_cast<int>(data.find('\n')) + 1}, g{[&] {
-          auto index = static_cast<int>(data.find('^'));
-          return Guard{index % dim, index / dim};
-        }()} {
-    Update(g, 'X');
+private:
+  std::vector<int> mapping_;
+  std::span<int> up_, right_, down_, left_;
+
+  [[nodiscard]] constexpr inline auto GetDir(this auto&& self, Dir id) noexcept {
+    return std::span{self.mapping_}.subspan(
+        self.dim * self.dim * static_cast<std::size_t>(std::to_underlying(id)), self.dim * self.dim);
   }
 
-  [[nodiscard]] constexpr char Map(Guard const& guard) const noexcept {
-    if (guard.y >= 0 and guard.y < dim and guard.x >= 0 and guard.x < dim) {
-      return buf[static_cast<std::uint32_t>(guard.y * dim + guard.x)];
-    } else {
-      return 0;
+public:
+  inline JumpMap(std::string_view grid)
+      : dim{grid.find('\n')},
+        guard{[&] {
+          auto [y, x] = std::div(static_cast<int>(grid.find('^')), static_cast<int>(dim + 1));
+          return Guard{Point{x, y}};
+        }()},
+        mapping_(dim * dim * 4),
+        up_{GetDir(Dir::Up)},
+        right_{GetDir(Dir::Right)},
+        down_{GetDir(Dir::Down)},
+        left_{GetDir(Dir::Left)} {
+    int const d{static_cast<int>(dim)};
+    auto ordered = std::views::iota(0, d);
+    for (int x : ordered) {
+      std::ranges::fold_left(ordered, -1, [&](int last, int y) {
+        Point const p{x, y};
+        if (grid[p.Index(dim + 1)] == '#') {
+          last = y + 1;
+        }
+        return up_[p.Index(dim)] = last;
+      });
+      std::ranges::fold_right(ordered, d, [&](int y, int last) {
+        Point const p{x, y};
+        if (grid[p.Index(dim + 1)] == '#') {
+          last = y - 1;
+        }
+        return down_[p.Index(dim)] = last;
+      });
+    }
+    for (int y : ordered) {
+      std::ranges::fold_left(ordered, -1, [&](int last, int x) {
+        Point const p{x, y};
+        if (grid[p.Index(dim + 1)] == '#') {
+          last = x + 1;
+        }
+        return left_[p.Index(dim)] = last;
+      });
+      std::ranges::fold_right(ordered, d, [&](int x, int last) {
+        Point const p{x, y};
+        if (grid[p.Index(dim + 1)] == '#') {
+          last = x - 1;
+        }
+        return right_[p.Index(dim)] = last;
+      });
     }
   }
 
-  constexpr void Update(Guard const& guard, char v) {
-    buf[static_cast<std::uint32_t>(guard.y * dim + guard.x)] = v;
+  [[nodiscard]] bool InBounds(Point const& pos) const noexcept {
+    return 0 <= pos.x and std::cmp_less(pos.x, dim) and 0 <= pos.y and std::cmp_less(pos.y, dim);
   }
 
-  [[nodiscard]] constexpr char LookAhead(Guard const& guard) const noexcept {
-    return Map(Guard{guard}.Move());
+  void Step(Guard& g) const noexcept {
+    std::size_t const index{g.loc.Index(dim)};
+    switch (g.dir) {
+    case Dir::Up:
+      g.loc.y = up_[index];
+      break;
+    case Dir::Down:
+      g.loc.y = down_[index];
+      break;
+    case Dir::Left:
+      g.loc.x = left_[index];
+      break;
+    case Dir::Right:
+      g.loc.x = right_[index];
+      break;
+    }
+    g.Turn();
+  }
+
+  void Step(Guard& g, Point const& obstacle) const noexcept {
+    auto& [pos, dir] = g;
+    Point const end{obstacle - dir};
+    switch (std::size_t const index{pos.Index(dim)}; dir) {
+    case Dir::Up: {
+      int const ny{up_[index]};
+      pos.y = (pos.x == obstacle.x and pos.y > obstacle.y and obstacle.y >= ny) ? end.y : ny;
+      break;
+    }
+    case Dir::Down: {
+      int const ny{down_[index]};
+      pos.y = (pos.x == obstacle.x and pos.y < obstacle.y and obstacle.y <= ny) ? end.y : ny;
+      break;
+    }
+    case Dir::Left: {
+      int const nx{left_[index]};
+      pos.x = (pos.y == obstacle.y and pos.x > obstacle.x and obstacle.x >= nx) ? end.x : nx;
+      break;
+    }
+    case Dir::Right: {
+      int const nx{right_[index]};
+      pos.x = (pos.y == obstacle.y and pos.x < obstacle.x and obstacle.x <= nx) ? end.x : nx;
+      break;
+    }
+    }
+    g.Turn();
+  }
+
+  [[nodiscard]] bool inline HasCycle(Guard g) const noexcept {
+    thread_local ankerl::unordered_dense::set<int> seen;
+    seen.clear();
+    Point const obstacle{g.loc + g.dir};
+    while (InBounds(g.loc)) {
+      std::size_t const index{g.Index(dim)};
+      if (auto [_, inserted] = seen.insert(index); not inserted) {
+        return true;
+      } else {
+        Step(g, obstacle);
+      }
+    }
+    return false;
   }
 };
 
-export using Day06ParsedType = Context;
-export using Day06AnswerType = int;
+template <std::ranges::random_access_range Range>
+void ParallelForEach(Range&& range,
+                     std::move_only_function<void(std::ranges::range_const_reference_t<Range>)> fn) {
+  static unsigned const THREADS{std::thread::hardware_concurrency()};
+  static dp::thread_pool<> thread_pool{THREADS};
+  std::size_t const n{range.size()};
+  long curr{0};
+  for (unsigned i : std::views::iota(0u, THREADS)) {
+    long const next{static_cast<long>((i + 1) * n / THREADS)};
+    thread_pool.enqueue_detach([&fn, &range, curr, next]() {
+      for (Guard& item : std::views::take(std::views::drop(range, curr), next - curr)) {
+        std::invoke(fn, item);
+      }
+    });
+    curr = next;
+  }
+  thread_pool.wait_for_tasks();
+}
+
+} // namespace
+
+export using Day06ParsedType = JumpMap;
+export using Day06AnswerType = long;
 
 export Day06ParsedType Day06Parse(std::string_view input) noexcept {
-  return Context{input};
+  return JumpMap{input};
 }
 
-constexpr auto Simulate = [](Context& ctx) {
-  int total{0};
-  Guard g{ctx.g};
-  while (true) {
-    if (ctx.Map(g) != 'x') {
-      ++total;
-      ctx.Update(g, 'x');
-    }
-    while (ctx.LookAhead(g) == '#') {
-      g.Turn();
-    }
-    if (ctx.LookAhead(g) == 0) {
-      break;
-    }
-    g.Move();
-  }
-  return total;
-};
+static std::vector<Guard> path;
 
-export Day06AnswerType Day06Part1(Day06ParsedType ctx) {
-  return Simulate(ctx);
+export Day06AnswerType Day06Part1(Day06ParsedType const& mapping) {
+  std::vector<bool> visited(static_cast<std::size_t>(mapping.dim * mapping.dim), false);
+  path.reserve(6'144);
+  path.clear();
+  Guard guard{mapping.guard};
+  while (mapping.InBounds(guard.loc)) {
+    auto const [start, dir] = guard;
+    mapping.Step(guard);
+    Point const stop{guard.loc};
+    for (Point p{start}; p != stop; p += dir) {
+      visited[p.Index(mapping.dim)] = true;
+      path.emplace_back(p, dir);
+    }
+  }
+  return std::ranges::count(visited, true);
 }
 
-export Day06AnswerType Day06Part2(Day06ParsedType ctx, [[maybe_unused]] Day06AnswerType const& answer) {
-  Simulate(ctx);
-  int total{0};
-  // std::unordered_set<Guard, GuardHash> history, shadow_history;
-  ankerl::unordered_dense::set<Guard, GuardHash> history, shadow_history;
-  Guard guard{ctx.g};
-  while (true) {
-    ctx.Update(guard, 'X');
-    char g;
-    while ((g = ctx.LookAhead(guard)) == '#') {
-      history.insert(guard);
-      guard.Turn();
-    }
-    if (g == 0) {
-      break;
-    } else if (g == 'x') {
-      Guard wall{guard};
-      wall.Move();
-      ctx.Update(wall, '#');
-      Guard shadow{guard};
-      shadow.Turn();
-      shadow_history.clear();
-      char s;
-      while ((s = ctx.LookAhead(shadow)) != 0) {
-        if (s == '#') {
-          if (history.contains(shadow) or shadow_history.contains(shadow)) {
-            break;
-          } else {
-            shadow_history.insert(shadow);
-          }
-          shadow.Turn();
-        } else {
-          shadow.Move();
-        }
-      }
-      total += (s != 0);
-      ctx.Update(wall, 'x');
-    }
-    guard.Move();
-  }
-  return total;
+export Day06AnswerType Day06Part2(Day06ParsedType const& mapping,
+                                  [[maybe_unused]] Day06AnswerType const& answer) {
+  std::vector<bool> visited(static_cast<std::size_t>(mapping.dim * mapping.dim), false);
+  std::atomic_long count{0};
+  ParallelForEach(path, [&](Guard const& guard) { count += mapping.HasCycle(guard); });
+  return count.load();
 }
