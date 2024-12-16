d1274af6f71572e74cd367f3beee0c2a90718e76
Will Killian <william.killian@outlook.com>
2024-12-15 22:53:33 -0500

    Day15: fix logic bug; speed improvements
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- src/day15.cpp
+++ src/day15.cpp
@@ -1,6 +1,8 @@
 module;
 
 #include <algorithm>
+#include <concepts>
+#include <functional>
 #include <ranges>
 #include <string>
 #include <string_view>
@@ -34,42 +36,48 @@ static constexpr auto Div = []<std::integral T>(T a, std::same_as<T> auto b) {
   return std::pair{a / b, a % b};
 };
 
+constexpr auto Expand = [] [[gnu::always_inline]] (char c) {
+  using std::string_view_literals::operator""sv;
+  switch (c) { // clang-format off
+    case 'O': return "[]"sv;
+    case '@': return "@."sv;
+    case '#': return "##"sv;
+    case '.': return ".."sv;
+  } // clang-format on
+  __builtin_unreachable();
+};
+
+static constexpr auto SkipNewline = std::bind_front(std::not_equal_to{}, '\n');
+
+static constexpr auto ToDir = [] [[gnu::always_inline]] (char m) {
+  switch (m) { // clang-format off
+    case '^': return Dir::Up;
+    case '>': return Dir::Right;
+    case 'v': return Dir::Down;
+    case '<': return Dir::Left;
+  } // clang-format on
+  __builtin_unreachable();
+};
+
 export Day15ParsedType Day15Parse(std::string_view input) noexcept {
+  using std::string_view_literals::operator""sv;
   int const width{static_cast<int>(input.find('\n'))};
   std::size_t const size{input.find("\n\n")};
   int const height{static_cast<int>(size + 1) / (width + 1)};
 
   std::string part1(static_cast<std::size_t>(width * height), '\0');
   std::string part2(static_cast<std::size_t>(width * height * 2), '\0');
-
-  for (auto i1 = part1.begin(), i2 = part2.begin();
-       char c : input.substr(0, static_cast<std::size_t>(size))) {
-    // clang-format off
-    switch (c) {
-      case 'O': *i1++ = 'O'; *i2++ = '['; *i2++ = ']'; break;
-      case '@': *i1++ = '@'; *i2++ = '@'; *i2++ = '.'; break;
-      case '#': *i1++ = '#'; *i2++ = '#'; *i2++ = '#'; break;
-      case '.': *i1++ = '.'; *i2++ = '.'; *i2++ = '.'; break;
-      default: break;
-    }
-    // clang-format on
-  }
+  auto const data = input.substr(0, static_cast<std::size_t>(size));
+  std::ranges::copy(std::views::filter(data, SkipNewline), part1.begin());
+  std::ranges::copy(std::views::join(std::views::transform(std::views::filter(data, SkipNewline), Expand)),
+                    part2.begin());
 
   std::string_view original_moves{input.substr(size + 2)};
   std::size_t const moves_width{original_moves.find('\n') + 1};
   std::size_t const move_rows{original_moves.size() / moves_width};
   std::vector<Dir> moves(original_moves.size() - move_rows);
-  for (auto i = moves.begin(); char m : original_moves) {
-    // clang-format off
-    switch (m) {
-      case '^': *i++ = Dir::Up; break;
-      case '>': *i++ = Dir::Right; break;
-      case 'v': *i++ = Dir::Down; break;
-      case '<': *i++ = Dir::Left; break;
-      default: break;
-    }
-    // clang-format on
-  }
+  std::ranges::copy(std::views::transform(std::views::filter(original_moves, SkipNewline), ToDir),
+                    moves.begin());
 
   int const offset{static_cast<int>(input.find('@'))};
   auto const [y, x] = Div(offset, width + 1);
@@ -99,130 +107,100 @@ static inline int Score(std::string_view grid, int width, char target) {
 
 export Day15AnswerType Day15Part1(Day15ParsedType const& simulation_data) noexcept {
   auto [grid, start, width] = simulation_data.part1;
-  auto loc = std::next(grid.begin(), start);
+  auto loc = std::next(std::begin(grid), start);
 
-  auto offset_for = [&](Dir d) {
-    static std::array arr{-width, 1, width, -1};
-    return arr[static_cast<unsigned char>(std::to_underlying(d))];
-  };
+  std::array const offsets{-width, 1, width, -1};
+  auto offset_for = [&](Dir d) { return offsets[static_cast<unsigned char>(std::to_underlying(d))]; };
 
-  for (Dir d : simulation_data.moves) {
-    int const offset{offset_for(d)};
-    auto check{std::next(loc, offset)};
+  for (int const offset : std::views::transform(simulation_data.moves, offset_for)) {
+    auto check{loc + offset};
     if (*check == '#') {
       continue;
     }
     if (*check == 'O') {
-      auto next{std::next(check, offset)};
+      auto next{check + offset};
       while (*next == 'O') {
-        std::advance(next, offset);
+        next += offset;
       }
       if (*next == '#') {
         continue;
       }
       *next = 'O';
     }
-    *check = '@';
-    *loc = '.';
-    loc = check;
+    *std::exchange(loc, check) = '.';
   }
+  *loc = '@';
   return Score(grid, width, 'O');
 }
 
 export Day15AnswerType Day15Part2(Day15ParsedType const& simulation_data,
                                   [[maybe_unused]] Day15AnswerType const& answer) {
   auto [grid, start, width] = simulation_data.part2;
-  auto loc = std::next(grid.begin(), start);
+  auto loc = std::next(std::begin(grid), start);
 
-  auto offset_for = [&](Dir d) {
-    static std::array arr{-width, 1, width, -1};
-    return arr[static_cast<unsigned char>(std::to_underlying(d))];
-  };
+  std::array const offsets{-width, 1, width, -1};
+  auto offset_for = [&](Dir d) { return offsets[static_cast<unsigned char>(std::to_underlying(d))]; };
 
   std::vector<std::string::iterator> boxes;
   boxes.reserve(40); // never was above 34 for my input
+  auto add_box = [&boxes] [[gnu::always_inline]] (std::string::iterator iter) {
+    if (*iter == '[') {
+      if (boxes.empty() or std::views::reverse(boxes)[1] != iter) {
+        boxes.push_back(iter);
+        boxes.push_back(iter + 1);
+      }
+    } else {
+      if (boxes.empty() or std::views::reverse(boxes)[0] != iter) {
+        boxes.push_back(iter - 1);
+        boxes.push_back(iter);
+      }
+    }
+  };
 
-  for (Dir d : simulation_data.moves) {
-    int const offset{offset_for(d)};
-    auto check{std::next(loc, offset)};
+  for (int const offset : std::views::transform(simulation_data.moves, offset_for)) {
+    auto check{loc + offset};
     if (*check == '#') {
       continue;
     }
     if (*check != '.') {
-      if (d == Dir::Left or d == Dir::Right) {
-        // Easy mode -- scan until we don't see a box
-
-        auto next{std::next(check, offset)};
+      if (offset & 1) {
+        auto next{check + offset};
         while (*next == '[' or *next == ']') {
-          std::advance(next, offset);
+          next += offset;
         }
-
-        // We encountered a wall -- abort
         if (*next == '#') {
           continue;
         }
-
-        // Next must have been a space
         while (next != loc) {
-          std::iter_swap(next, std::prev(next, offset));
-          std::advance(next, -offset);
+          std::iter_swap(next, next -= offset);
         }
       } else {
-        // Hard mode -- potentially need to move many (or no) boxes
-
-        // Provide a convenience function to enqueue new boxes to consider
-        auto add_box = [&boxes] [[gnu::always_inline]] (std::string::iterator iter) {
-          if (*iter == '[') {
-            if (std::views::reverse(boxes)[1] != iter) {
-              boxes.push_back(iter);
-              boxes.push_back(std::next(iter));
-            }
-          } else {
-            if (std::views::reverse(boxes)[0] != iter) {
-              boxes.push_back(std::prev(iter));
-              boxes.push_back(iter);
-            }
-          }
-        };
-
-        // Enqueue all boxes that should move
         [&] {
           add_box(check);
-          // While we have more to check
-          for (auto prev = boxes.begin(); prev != boxes.end();) {
-            auto end = boxes.end();
+          for (std::contiguous_iterator auto prev = boxes.begin(); prev != boxes.end();) {
+            std::contiguous_iterator auto end = boxes.end();
             for (auto const box : std::ranges::subrange(prev, end)) {
-              if (auto const next{std::next(box, offset)}; *next == '.') [[likely]] {
-                // Nothing to do, but still in a valid state!
-              } else if (*next == '#') {
-                // Encountered a wall -- abort immediately
-                boxes.clear();
-                return;
-              } else {
-                // Add one new box (two iterators)
-                add_box(next);
+              if (auto const next{box + offset}; *next != '.') {
+                if (*next == '#') {
+                  boxes.clear();
+                  return;
+                } else {
+                  add_box(next);
+                }
               }
             }
             prev = end;
           }
         }();
-
-        // This means we hit a wall and should abort
         if (boxes.empty()) {
           continue;
         }
-
-        // Move all of the boxes
-        std::ranges::for_each(std::views::reverse(boxes),
-                              [&](auto box) { std::iter_swap(box, std::next(box, offset)); });
+        std::ranges::for_each(std::views::reverse(boxes), [&](auto b) { std::iter_swap(b, b + offset); });
         boxes.clear();
       }
     }
-
-    // Perform the move
-    *check = '@';
-    *loc = '.';
-    loc = check;
+    *std::exchange(loc, check) = '.';
   }
+  *loc = '@';
   return Score(grid, width, '[');
 }
